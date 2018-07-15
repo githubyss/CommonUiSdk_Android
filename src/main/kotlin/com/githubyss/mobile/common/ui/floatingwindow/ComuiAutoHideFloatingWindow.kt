4ecbdb03f5b732267caef612953b9409400f0437
syan <hotyss@hotmail.com>
2018-07-16 00:17:36 +0800

    1) New floating window bg drawable; 2) Update window show and hide fun in floating window

--- src/main/kotlin/com/githubyss/mobile/common/ui/floatingwindow/ComuiAutoHideFloatingWindow.kt
+++ src/main/kotlin/com/githubyss/mobile/common/ui/floatingwindow/ComuiAutoHideFloatingWindow.kt
@@ -1,10 +1,10 @@
 package com.githubyss.mobile.common.ui.floatingwindow
 
 import android.animation.Animator
-import android.animation.ObjectAnimator
 import android.animation.ValueAnimator
 import android.app.Application
 import android.content.Context
+import android.graphics.PixelFormat
 import android.os.Handler
 import android.os.Message
 import android.support.annotation.RequiresPermission
@@ -63,19 +63,18 @@ class ComuiAutoHideFloatingWindow private constructor() : View.OnClickListener, 
 
     private val FAULT_VALUE = -1
 
-    //    private var windowView: ViewGroup? = null
     private var rootView: ViewGroup? = null
 
-    private var ivCenter: ImageView? = null
-    private var btnCenter: Button? = null
-    private var llTouchMove: LinearLayout? = null
+    private var viewHolder: ViewHolder? = null
 
-    private var slideDownAnimator: ObjectAnimator? = null
-    private var slideUpAnimator: ObjectAnimator? = null
+//    private var slideDownAnimator: ObjectAnimator? = null
+//    private var slideUpAnimator: ObjectAnimator? = null
+
+    private var slideDownAnimator: ValueAnimator? = null
+    private var slideUpAnimator: ValueAnimator? = null
 
     private var windowManager: WindowManager? = null
     private var windowLayoutParams: WindowManager.LayoutParams? = null
-//    private var rootLayoutParams: FrameLayout.LayoutParams? = null
 
     private var beShown = false
     private var beAnimating = false
@@ -83,13 +82,17 @@ class ComuiAutoHideFloatingWindow private constructor() : View.OnClickListener, 
     private var beSlidingUp = false
 
     private var rootViewMeasuredHeight = FAULT_VALUE
-    private var rootViewCoordinateY = FAULT_VALUE
     private var moveBeginCoordinateY = FAULT_VALUE
     private var moveCurrentCoordinateY = FAULT_VALUE
     private var moveDeltaCoordinateY = FAULT_VALUE
 
     private var autoHideHandler: AutoHideHandler? = null
     private var autoHideRunnable: AutoHideRunnable? = null
+
+
+    init {
+        initLayoutParams()
+    }
 
 
     private class AutoHideHandler constructor(private val autoHideFloatingWindowWeakRef: WeakReference<ComuiAutoHideFloatingWindow>) : Handler() {
@@ -108,6 +111,12 @@ class ComuiAutoHideFloatingWindow private constructor() : View.OnClickListener, 
                 autoHideHandler?.sendMessage(autoHideHandler?.obtainMessage(MSG_HIDE_FLOATING_WINDOW))
             }
         }
+    }
+
+    private inner class ViewHolder {
+        var ivCenter: ImageView? = null
+        var btnCenter: Button? = null
+        var llTouchMove: LinearLayout? = null
     }
 
 
@@ -153,7 +162,7 @@ class ComuiAutoHideFloatingWindow private constructor() : View.OnClickListener, 
                 if ((windowLayoutParams?.y ?: 0) < 0) {
                     hide()
                 } else {
-
+                    autoHide(true)
                 }
 
                 logcatViewProperty(rootView, "ACTION_UP rootView\t\t")
@@ -163,6 +172,8 @@ class ComuiAutoHideFloatingWindow private constructor() : View.OnClickListener, 
                 slideDownAnimator?.cancel()
                 slideUpAnimator?.cancel()
                 autoHideHandler?.removeCallbacks(autoHideRunnable)
+
+                logcatViewProperty(rootView, "ACTION_UP rootView\t\t")
             }
         }
 
@@ -199,6 +210,11 @@ class ComuiAutoHideFloatingWindow private constructor() : View.OnClickListener, 
     }
 
     override fun onAnimationUpdate(animation: ValueAnimator?) {
+        val currentValue = animation?.animatedValue as Float
+        if ((windowLayoutParams?.y ?: FAULT_VALUE) >= 0) rootView?.y = 0F else rootView?.y = currentValue
+        windowLayoutParams?.y = currentValue.toInt()
+        windowManager?.updateViewLayout(rootView, windowLayoutParams)
+
         logcatViewProperty(rootView, "onAnimationUpdate rootView\t\t")
     }
 
@@ -206,8 +222,6 @@ class ComuiAutoHideFloatingWindow private constructor() : View.OnClickListener, 
     @RequiresPermission(android.Manifest.permission.SYSTEM_ALERT_WINDOW)
     fun show(application: Application): ComuiAutoHideFloatingWindow {
         inflateView(application)
-        initView(rootView)
-        initLayoutParams()
         addView()
         slideDown()
         return this@ComuiAutoHideFloatingWindow
@@ -219,30 +233,11 @@ class ComuiAutoHideFloatingWindow private constructor() : View.OnClickListener, 
     }
 
     fun setImage(path: String): ComuiAutoHideFloatingWindow {
-        ivCenter ?: return this@ComuiAutoHideFloatingWindow
+        viewHolder?.ivCenter ?: return this@ComuiAutoHideFloatingWindow
         Glide.with(ComkitApplication.instance.application)
                 .load(path)
-                .into(ivCenter ?: return this@ComuiAutoHideFloatingWindow)
+                .into(viewHolder?.ivCenter ?: return this@ComuiAutoHideFloatingWindow)
         return this@ComuiAutoHideFloatingWindow
-    }
-
-    private fun inflateView(context: Context) {
-        val inflater = LayoutInflater.from(context)
-        if (rootView == null) {
-            rootView = inflater.inflate(R.layout.comui_auto_hide_floating_window, null, false) as FrameLayout
-        }
-
-        rootView?.measure(View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED), View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED))
-        rootViewMeasuredHeight = rootView?.measuredHeight ?: 0
-        logcatViewProperty(rootView, "After measure rootView\t\t\t")
-    }
-
-    private fun initView(view: View?) {
-        ivCenter = view?.findViewById(R.id.ivCenter)
-        btnCenter = view?.findViewById(R.id.btnCenter)
-        llTouchMove = view?.findViewById(R.id.llTouchMove)
-        btnCenter?.setOnClickListener(this@ComuiAutoHideFloatingWindow)
-        llTouchMove?.setOnTouchListener(this@ComuiAutoHideFloatingWindow)
     }
 
     private fun initLayoutParams() {
@@ -257,14 +252,39 @@ class ComuiAutoHideFloatingWindow private constructor() : View.OnClickListener, 
         windowLayoutParams?.type = WindowManager.LayoutParams.TYPE_SYSTEM_ERROR
         windowLayoutParams?.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE or WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL or WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN or WindowManager.LayoutParams.FLAG_LAYOUT_INSET_DECOR or WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH
         windowLayoutParams?.gravity = Gravity.TOP or Gravity.CENTER_HORIZONTAL
-//        windowLayoutParams?.format = PixelFormat.TRANSLUCENT
-        windowLayoutParams?.alpha = 0.9F
+
+        /** You can comment configuration of format to display the black window manager. by Ace Yan */
+        windowLayoutParams?.format = PixelFormat.TRANSLUCENT
+        /** Config alpha of window manager. by Ace Yan */
+//        windowLayoutParams?.alpha = 0.9F
         windowLayoutParams?.width = WindowManager.LayoutParams.MATCH_PARENT
         windowLayoutParams?.height = WindowManager.LayoutParams.WRAP_CONTENT
 
-        /* The x here means the offset to the gravity, and will be ignored when the gravity is default. Details see in "https://developer.android.google.cn/reference/android/view/WindowManager.LayoutParams" */
+        /** The x here means the offset to the gravity, and will be ignored when the gravity is default. Details see in "https://developer.android.google.cn/reference/android/view/WindowManager.LayoutParams". by Ace Yan */
         windowLayoutParams?.x = 0
         windowLayoutParams?.y = 0
+    }
+
+    private fun inflateView(context: Context) {
+        val inflater = LayoutInflater.from(context)
+        if (rootView == null) {
+            rootView = inflater.inflate(R.layout.comui_auto_hide_floating_window, null, false) as FrameLayout
+            initView(rootView)
+        }
+
+        rootView?.measure(View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED), View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED))
+        rootViewMeasuredHeight = rootView?.measuredHeight ?: FAULT_VALUE
+        logcatViewProperty(rootView, "After measure rootView\t\t\t")
+    }
+
+    private fun initView(view: View?) {
+        viewHolder = ViewHolder()
+        viewHolder?.ivCenter = view?.findViewById(R.id.ivCenter)
+        viewHolder?.btnCenter = view?.findViewById(R.id.btnCenter)
+        viewHolder?.llTouchMove = view?.findViewById(R.id.llTouchMove)
+
+        viewHolder?.btnCenter?.setOnClickListener(this@ComuiAutoHideFloatingWindow)
+        viewHolder?.llTouchMove?.setOnTouchListener(this@ComuiAutoHideFloatingWindow)
     }
 
     private fun addView() {
@@ -294,69 +314,72 @@ class ComuiAutoHideFloatingWindow private constructor() : View.OnClickListener, 
     }
 
     private fun releaseView() {
-        ivCenter?.setImageResource(0)
+        viewHolder?.ivCenter?.setImageResource(0)
     }
 
     private fun slideDown() {
-        initSlideDownAnimator(rootView)
-        if (beShown && !beAnimating) {
+        if (beShown && !beAnimating && initSlideDownAnimator(rootView)) {
             slideDownAnimator?.start()
             beSlidingDown = true
         }
     }
 
     private fun slideUp() {
-        initSlideUpAnimator(rootView)
-        if (beShown && !beAnimating) {
+        if (beShown && !beAnimating && initSlideUpAnimator(rootView)) {
             slideUpAnimator?.start()
             beSlidingUp = true
         }
     }
 
-    private fun initSlideDownAnimator(view: View?) {
-        val beginY: Float
-        val endY: Float
-        rootView?.y = -rootViewMeasuredHeight.toFloat()
-        when (rootViewCoordinateY) {
-            FAULT_VALUE -> {
-                beginY = -rootViewMeasuredHeight.toFloat()
-                endY = 0F
-            }
+    private fun floating() {
+    }
 
-            else -> {
-                beginY = -(rootViewCoordinateY + rootViewMeasuredHeight).toFloat()
-                endY = 0F
-            }
-        }
-        slideDownAnimator = ObjectAnimator.ofFloat(view, "translationY", -rootViewMeasuredHeight.toFloat(), 0F)
+//    private fun initSlideDownAnimator(view: View?) {
+//        slideDownAnimator = ObjectAnimator.ofFloat(view, "y", -rootViewMeasuredHeight.toFloat(), 0F)
+//        slideDownAnimator?.duration = ANIMATION_DURATION
+//        slideDownAnimator?.addListener(this@ComuiAutoHideFloatingWindow)
+//        slideDownAnimator?.addUpdateListener(this@ComuiAutoHideFloatingWindow)
+//
+//        logcatViewProperty(rootView, "initSlideDownAnimator rootView\t")
+//    }
+//
+//    private fun initSlideUpAnimator(view: View?) {
+//        slideUpAnimator = ObjectAnimator.ofFloat(view, "y", 0F, -rootViewMeasuredHeight.toFloat())
+//        slideUpAnimator?.duration = ANIMATION_DURATION
+//        slideUpAnimator?.addListener(this@ComuiAutoHideFloatingWindow)
+//        slideUpAnimator?.addUpdateListener(this@ComuiAutoHideFloatingWindow)
+//
+//        logcatViewProperty(rootView, "initSlideUpAnimator rootView\t")
+//    }
+
+    private fun initSlideDownAnimator(view: View?): Boolean {
+        if (rootViewMeasuredHeight == FAULT_VALUE) return false
+
+        val beginY = -rootViewMeasuredHeight.toFloat()
+        val endY = 0F
+        slideDownAnimator = ValueAnimator.ofFloat(beginY, endY)
         slideDownAnimator?.duration = ANIMATION_DURATION
         slideDownAnimator?.addListener(this@ComuiAutoHideFloatingWindow)
         slideDownAnimator?.addUpdateListener(this@ComuiAutoHideFloatingWindow)
 
         logcatViewProperty(rootView, "initSlideDownAnimator rootView\t")
+        return true
     }
 
-    private fun initSlideUpAnimator(view: View?) {
-        val beginY: Float
-        val endY: Float
-        when (rootViewCoordinateY) {
-            FAULT_VALUE -> {
-                beginY = rootView?.y ?: 0F
-                endY = -((rootView?.y ?: 0F) + (rootView?.measuredHeight ?: 0))
-            }
+    private fun initSlideUpAnimator(view: View?): Boolean {
+        if (rootViewMeasuredHeight == FAULT_VALUE) return false
 
-            else -> {
-                beginY = rootViewCoordinateY.toFloat()
-//                endY =
-            }
-        }
-
-        slideUpAnimator = ObjectAnimator.ofFloat(view, "translationY", 0F, -rootViewMeasuredHeight.toFloat())
+        val coordinate = IntArray(2)
+        view?.getLocationOnScreen(coordinate)
+        val beginY = coordinate[1].toFloat()
+        val endY = -rootViewMeasuredHeight.toFloat()
+        slideUpAnimator = ValueAnimator.ofFloat(beginY, endY)
         slideUpAnimator?.duration = ANIMATION_DURATION
         slideUpAnimator?.addListener(this@ComuiAutoHideFloatingWindow)
         slideUpAnimator?.addUpdateListener(this@ComuiAutoHideFloatingWindow)
 
         logcatViewProperty(rootView, "initSlideUpAnimator rootView\t")
+        return true
     }
 
     private fun autoHide(canAutoHide: Boolean) {
@@ -380,8 +403,7 @@ class ComuiAutoHideFloatingWindow private constructor() : View.OnClickListener, 
                         "{t:${view?.top}, b:${view?.bottom}, l:${view?.left}, r:${view?.right}}\t" +
                         "params-> " +
                         "{x:${windowLayoutParams?.x}, y:${windowLayoutParams?.y}}\t" +
-                        "{w:${windowLayoutParams?.width}, h:${windowLayoutParams?.height}}\t"
-        )
+                        "{w:${windowLayoutParams?.width}, h:${windowLayoutParams?.height}}\t")
     }
 
 //    private fun logcatCoordinate(location: String = "") {
