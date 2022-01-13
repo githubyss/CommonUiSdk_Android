e67bb565efd1c2d4e8c147e79c65ede9104de948
ace <hotyss@hotmail.com>
2022-01-13 17:58:37 +0800

    1）优化build.gradle的多渠道打包配置；2）处理AndroidManifest关于label重复的问题。

--- src/alone/kotlin/com/githubyss/mobile/common/ui/alone/page/speech_recognition/widget/RecordImageButton.kt
+++ src/alone/kotlin/com/githubyss/mobile/common/ui/alone/page/speech_recognition/widget/RecordImageButton.kt
@@ -2,12 +2,12 @@ package com.githubyss.mobile.common.ui.alone.page.speech_recognition.widget
 
 import android.content.Context
 import android.util.AttributeSet
-import android.util.Log
 import android.view.MotionEvent
 import android.view.View
 import android.widget.ProgressBar
 import android.widget.RelativeLayout
 import android.widget.TextView
+import com.githubyss.mobile.common.kit.util.LogUtils
 import com.githubyss.mobile.common.ui.R
 
 
