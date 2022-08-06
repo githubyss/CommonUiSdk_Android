54bf138750c8a24cbcb029a765b682ea7a0053e7
ace <hotyss@hotmail.com>
2022-08-06 22:16:33 +0800

    1）文章删除添加二次确认弹框；2）文章编辑列表布局优化

--- src/app/kotlin/com/githubyss/mobile/common/ui/app/page/recycler_view/RecyclerViewFragment.kt
+++ src/app/kotlin/com/githubyss/mobile/common/ui/app/page/recycler_view/RecyclerViewFragment.kt
@@ -2,8 +2,10 @@ package com.githubyss.mobile.common.ui.app.page.recycler_view
 
 import androidx.fragment.app.viewModels
 import com.githubyss.mobile.common.kit.base.activity_fragment.binding_reflect_view_model.BaseReflectBindingViewModelToolbarFragment
+import com.githubyss.mobile.common.kit.util.showToast
 import com.githubyss.mobile.common.ui.R
 import com.githubyss.mobile.common.ui.databinding.ComuiFragmentRecyclerViewBinding
+import com.githubyss.mobile.common.ui.dialog.hint.HintDialog
 import com.githubyss.mobile.common.ui.recycler_view.base.binding.BindingAdapterDoubleLayerItem
 import com.githubyss.mobile.common.ui.recycler_view.base.binding.BindingAdapterItem
 import com.githubyss.mobile.common.ui.recycler_view.base.binding.BindingRecyclerViewAdapter
@@ -108,7 +110,17 @@ class RecyclerViewFragment : BaseReflectBindingViewModelToolbarFragment<ComuiFra
 
     /**  */
     fun onButtonDeleteClick() {
-        recyclerViewVm.deleteItemTitle()
-        bindingRecyclerViewAdapter.updateDataList(recyclerViewVm.itemsCombineEditing)
+        HintDialog.instance.showDialog(
+            parentFragmentManager,
+            "确认删除", "${recyclerViewVm.ids.value?.size} 篇内容将从您的听单列表中删除，是否确认删除？",
+            "确认", "取消",
+            {
+                showToast("删除")
+                recyclerViewVm.deleteItemTitle()
+                bindingRecyclerViewAdapter.updateDataList(recyclerViewVm.itemsCombineEditing)
+            },
+            {
+                showToast("取消")
+            })
     }
 }
