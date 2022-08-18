d08e5b248822b7c89f09ce23e6b11d74133c72d5
ace <hotyss@hotmail.com>
2022-08-18 20:51:22 +0800

    删除多余的类

--- src/app/kotlin/com/githubyss/mobile/common/ui/app/Main.java
+++
@@ -1,7 +0,0 @@
-package com.githubyss.mobile.common.ui.app;
-
-
-class Main {
-    void main() {
-    }
-}
--- src/app/kotlin/com/githubyss/mobile/common/ui/app/Main.kt
+++
@@ -1,5 +0,0 @@
-package com.githubyss.mobile.common.ui.app
-
-
-fun main(args: Array<String>) {
-}
