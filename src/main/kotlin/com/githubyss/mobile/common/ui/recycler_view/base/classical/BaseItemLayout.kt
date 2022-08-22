7796b7f27c1cd56cefaea812d1871761dd9a2d0f
ace <hotyss@hotmail.com>
2022-08-22 19:30:48 +0800

    更新导入

--- src/main/kotlin/com/githubyss/mobile/common/ui/recycler_view/base/classical/BaseItemLayout.kt
+++ src/main/kotlin/com/githubyss/mobile/common/ui/recycler_view/base/classical/BaseItemLayout.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.View
 import androidx.databinding.ViewDataBinding
-import androidx.viewbinding.ViewBinding
 import com.githubyss.common.base.frame_layout.binding_reflect.BaseReflectBindingFrameLayout
 
 
--- src/main/kotlin/com/githubyss/mobile/common/ui/toolbar/classical/NormalToolbar.kt
+++ src/main/kotlin/com/githubyss/mobile/common/ui/toolbar/classical/NormalToolbar.kt
@@ -2,12 +2,8 @@ package com.githubyss.mobile.common.ui.toolbar.classical
 
 import android.content.Context
 import android.util.AttributeSet
-import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModelProvider
 import com.githubyss.common.base.frame_layout.binding_reflect.BaseReflectBindingFrameLayout
 import com.githubyss.mobile.common.ui.databinding.ComuiToolbarNormalBinding
-import com.githubyss.mobile.common.ui.dialog.hint.HintDialogVm
-import com.githubyss.mobile.common.ui.time_view.classical.TimeCountdownView
 
 
 class NormalToolbar @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : BaseReflectBindingFrameLayout<ComuiToolbarNormalBinding>(context, attrs, defStyleAttr) {
