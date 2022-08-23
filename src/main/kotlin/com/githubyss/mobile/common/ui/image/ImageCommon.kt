804e130ee85dfc9975c1b143efcfd3e53879af81
ace <hotyss@hotmail.com>
2022-08-23 15:21:50 +0800

    1）新增图片控件的 Compose 通用实现；2）新增横竖向布局的通用实现

---
+++ src/main/kotlin/com/githubyss/mobile/common/ui/image/ImageCommon.kt
@@ -0,0 +1,13 @@
+package com.githubyss.mobile.common.ui.image
+
+import androidx.compose.foundation.Image
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.res.painterResource
+
+
+@Composable
+fun ImageCommon(
+    path: String
+) {
+}
--- src/main/kotlin/com/githubyss/mobile/common/ui/toolbar/classical/NormalToolbarVm.kt
+++ src/main/kotlin/com/githubyss/mobile/common/ui/toolbar/classical/NormalToolbarVm.kt
@@ -19,9 +19,9 @@ class NormalToolbarVm : ViewModel() {
 
     /**  */
     companion object {
-        const val TEXT_DEFAULT = ""
-        const val ACTION_LEFT_DEFAULT = R.drawable.comres_icon_close_no_border
-        const val ACTION_RIGHT_DEFAULT = R.drawable.comres_icon_close_no_border
+        val TEXT_DEFAULT = ""
+        val ACTION_LEFT_DEFAULT = R.drawable.comres_icon_close_no_border
+        val ACTION_RIGHT_DEFAULT = R.drawable.comres_icon_close_no_border
     }
 
 
